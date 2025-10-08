50703a1b62c776ecc1cbae4d427407e4a2e76c97
Joshua Job <joshuasjob@hotmail.com>
2025-10-08 14:57:21 -0600

    Completed server.delete()

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,21 +4,21 @@ import io.javalin.*;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
 
+        server.delete("db", ctx -> ctx.result("{}"));
         // Register your endpoints and exception handlers here.
 
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
-    public void stop() {
-        javalin.stop();
+    public void stop() {server.stop();
     }
 }
