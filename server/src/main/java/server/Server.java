a0568f18fa45d83cb1fed0af0bb31c75f03b01c1
Joshua Job <joshuasjob@hotmail.com>
2025-10-13 23:27:58 -0600

    Register can serialize and deserialize data

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,12 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+
+import io.javalin.http.Context;
+import model.UserData;
+
+import java.util.HashMap;
 
 public class Server {
 
@@ -10,8 +16,23 @@ public class Server {
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
         server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", this::register);
         // Register your endpoints and exception handlers here.
 
+    }
+
+    private void register(Context ctx){
+        var serializer = new Gson();
+
+        var data = new UserData("a", "b", "c");
+
+        // serialize to JSON
+        var json = serializer.toJson(data);
+
+        // deserialize back to ChessGame
+        var req = serializer.fromJson(json, UserData.class);
+
+        ctx.result("{\"username\":\"joe\", \"authToken\":\"xyz\"}");
     }
 
     public int run(int desiredPort) {
@@ -19,6 +40,5 @@ public class Server {
         return server.port();
     }
 
-    public void stop() {server.stop();
-    }
+    public void stop() {server.stop();}
 }
