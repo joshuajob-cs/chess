fae1c37184a8a812dfc66fa97a8fe21091029e69
Joshua Job <joshuasjob@hotmail.com>
2025-10-23 00:26:49 -0600

    More test cases

--- server/src/test/java/service/ServiceTest.java
+++ server/src/test/java/service/ServiceTest.java
@@ -1,7 +1,9 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.*;
 import model.CreateGameRequest;
+import model.JoinGameRequest;
 import model.LoginData;
 import model.UserData;
 import org.junit.jupiter.api.Test;
@@ -83,21 +85,85 @@ public class ServiceTest {
 
     @Test
     void logout() throws DataAccessException{
+        var userMemory = new MemoryUserDAO();
+        AuthDAO authMemory = new MemoryAuthDAO();
+        var userService = new UserService();
+        var credentials = userService.register(new UserData("out", "12345", "j@j"));
+        assertNotNull(userMemory.getUser(credentials.username()));
+        credentials = userService.login(new LoginData("out", "12345"));
+        assertNotNull(authMemory.getAuth(credentials.authToken()));
+        userService.logout(credentials.authToken());
+        assertNull(authMemory.getAuth(credentials.authToken()));
+    }
 
+    @Test
+    void logoutNeg() throws DataAccessException {
+        var userService = new UserService();
+        assertThrows(DataAccessException.class, () -> userService.logout("Non-token"));
     }
 
     @Test
     void listGames() throws DataAccessException{
+        var clearer = new ClearService();
+        clearer.clearAll();
+        var userService = new UserService();
+        var gameService = new GameService();
+        var credentials = userService.register(new UserData("gamer", "12345", "j@j"));
+        gameService.createGame(new CreateGameRequest(credentials.authToken(), "semi-finals"));
+        var listOfGames = gameService.listGames(credentials.authToken());
+        assertEquals(1, listOfGames.games().size());
+    }
 
+    @Test
+    void listGamesNeg() throws DataAccessException{
+        var clearer = new ClearService();
+        clearer.clearAll();
+        var userService = new UserService();
+        var gameService = new GameService();
+        var credentials = userService.register(new UserData("gamer", "12345", "j@j"));
+        var listOfGames = gameService.listGames(credentials.authToken());
+        assertNotEquals(1, listOfGames.games().size());
     }
 
     @Test
     void createGame() throws DataAccessException{
+        var gameMemory = new MemoryGameDAO();
+        var userService = new UserService();
+        var gameService = new GameService();
+        var credentials = userService.register(new UserData("master", "12345", "j@j"));
+        var gameID = gameService.createGame(new CreateGameRequest(credentials.authToken(), "fun-times"));
+        assertNotNull(gameMemory.getGame(gameID));
+    }
 
+    @Test
+    void createGameNeg() throws DataAccessException{
+        var userService = new UserService();
+        var gameService = new GameService();
+        var credentials = userService.register(new UserData("master", "12345", "j@j"));
+        assertThrows(DataAccessException.class, () -> gameService.createGame(new CreateGameRequest(credentials.authToken(), null)));
     }
 
     @Test
     void joinGame() throws DataAccessException{
+        var gameMemory = new MemoryGameDAO();
+        var userService = new UserService();
+        var gameService = new GameService();
+        var credentials = userService.register(new UserData("winner", "12345", "j@j"));
+        var gameID = gameService.createGame(new CreateGameRequest(credentials.authToken(), "fun-times"));
+        assertNotNull(gameMemory.getGame(gameID));
+        gameService.joinGame(new JoinGameRequest(credentials.authToken(), ChessGame.TeamColor.WHITE, gameID));
+        assertEquals("winner", gameMemory.getGame(gameID).whiteUsername());
+    }
 
+    @Test
+    void joinGameNeg() throws DataAccessException{
+        var gameMemory = new MemoryGameDAO();
+        var userService = new UserService();
+        var gameService = new GameService();
+        var credentials = userService.register(new UserData("king", "12345", "j@j"));
+        var gameID = gameService.createGame(new CreateGameRequest(credentials.authToken(), "fun-times"));
+        assertNotNull(gameMemory.getGame(gameID));
+        gameService.joinGame(new JoinGameRequest(credentials.authToken(), ChessGame.TeamColor.BLACK, gameID));
+        assertNotEquals("king", gameMemory.getGame(gameID).whiteUsername());
     }
 }
