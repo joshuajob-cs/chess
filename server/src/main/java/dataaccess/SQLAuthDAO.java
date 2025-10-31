0d12b51eaf64d8fd3f18a950280db334c8b8634c
Joshua Job <joshuasjob@hotmail.com>
2025-10-31 00:59:22 -0600

    SQL createAuth

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -5,6 +5,7 @@ import model.AuthData;
 import java.sql.SQLException;
 
 import static dataaccess.SQLUtilities.SQLClear;
+import static dataaccess.SQLUtilities.exists;
 
 public class SQLAuthDAO implements AuthDAO{
 
@@ -15,7 +16,20 @@ public class SQLAuthDAO implements AuthDAO{
 
     @Override
     public void createAuth(AuthData data) throws DataAccessException {
-
+        if(exists(data.authToken(), "authToken", "auth")){
+            throw new DataAccessException("403", new DataAccessException("Error: already taken"));
+        }
+        try(var conn = DatabaseManager.getConnection()) {
+            try(var statement = conn.prepareStatement(
+                    "INSERT INTO user " +
+                            "VALUES (?, ?);")){
+                statement.setString(1, data.authToken());
+                statement.setString(2, data.username());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
